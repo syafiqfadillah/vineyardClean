62aefcf68ee542d2aea6b2835c4dc9acd49f3c7d
infinitedim <saputradimas.boediluhur@gmail.com>
2022-10-19 22:22:13 +0700

    finish navbar and responsive

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./sass/root.sass";
+
+const main = () => {
+  const menu = document.querySelector(".ri-menu-line");
+  const list = document.querySelector(".nav-list");
+
+  menu.addEventListener("click", () => {
+    list.classList.toggle("slide");
+    menu.classList.toggle("black");
+  });
+};
+
+main();
