eb1103a1bca54b5a10f6aa632b27f355271a9914
syafiqfadillah <codingan77@gmail.com>
2022-10-22 05:42:40 +0700

    add functionality to the report form

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import "./sass/root.sass";
 
+document.getElementById("send").addEventListener("click", () => {
+  const data = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    sentences: document.getElementById("sentences").value
+  }
+
+  emailjs.send("contact_service", "contact_form", data).then(res => {
+      console.log("test");
+  })
+})
+
 const main = () => {
   const menu = document.querySelector(".ri-menu-line");
   const list = document.querySelector(".nav-list");
