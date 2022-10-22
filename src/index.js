7254566b29fc71d8c8aa8965bfea701feac71f95
infinitedim <saputradimas.boediluhur@gmail.com>
2022-10-22 20:22:28 +0700

    fix some bug

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import "./sass/root.sass";
 // Fungsi main untuk menjalankan semua program yang ada di dalamnya
 const main = () => {
   // deklarasi variabel untuk mengambil element dari html
-  const menu = document.querySelector(".ri-menu-line");
+  const menu = document.querySelector(".fa-burger");
   const list = document.querySelector(".nav-list");
-	const data = {
+  const data = {
     name: document.getElementById("name").value,
     email: document.getElementById("email").value,
-    sentences: document.getElementById("sentences").value
-  }
+    sentences: document.getElementById("sentences").value,
+  };
 
   // Event listener ketika menu di klik
   menu.addEventListener("click", () => {
@@ -18,13 +18,12 @@ const main = () => {
     menu.classList.toggle("black");
     document.body.classList.toggle("hidden");
   });
-	
-	document.getElementById("send").addEventListener("click", () => {
 
-  emailjs.send("contact_service", "contact_form", data).then(res => {
+  document.getElementById("send").addEventListener("click", () => {
+    emailjs.send("contact_service", "contact_form", data).then((res) => {
       console.log("test");
-  })
-})
+    });
+  });
 };
 
 // Jalankan fungi main
