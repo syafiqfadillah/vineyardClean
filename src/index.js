0ea370a217adb403e3975a9cfab2f74594c61045
infinitedim <saputradimas.boediluhur@gmail.com>
2022-10-22 07:05:17 +0700

    refactor

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
+// import package yang dibutuhkan
 import "./sass/root.sass";
 
+// Fungsi main untuk menjalankan semua program yang ada di dalamnya
 const main = () => {
+  // deklarasi variabel untuk mengambil element dari html
   const menu = document.querySelector(".ri-menu-line");
   const list = document.querySelector(".nav-list");
 
+  // Event listener ketika menu di klik
   menu.addEventListener("click", () => {
     list.classList.toggle("slide");
     menu.classList.toggle("black");
@@ -11,4 +15,5 @@ const main = () => {
   });
 };
 
+// Jalankan fungi main
 main();
