57d3b9c99dd8d43a89460b00d0739b7d294e953c
infinitedim <saputradimas.boediluhur@gmail.com>
2022-10-23 19:22:53 +0700

    add additional package

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 // import package yang dibutuhkan
+import Swal from "sweetalert2";
 import "./sass/root.sass";
 
 // Fungsi main untuk menjalankan semua program yang ada di dalamnya
@@ -7,12 +8,6 @@ const main = () => {
   const menu = document.querySelector(".fa-burger");
   const list = document.querySelector(".nav-list");
 
-  const data = {
-    name: document.getElementById("name").value,
-    email: document.getElementById("email").value,
-    sentences: document.getElementById("sentences").value,
-  };
-
   // Event listener ketika menu di klik
   menu.addEventListener("click", () => {
     list.classList.toggle("slide");
@@ -20,35 +15,42 @@ const main = () => {
     document.body.classList.toggle("hidden");
   });
 
-	document.getElementById("send").addEventListener("click", (e) => {
-    e.preventDefault()
+  document.getElementById("send").addEventListener("click", (e) => {
+    e.preventDefault();
     const data = {
-      service_id: "contact_service",
-      template_id: "contact_form",
-      user_id: "l2XE1auQUF2SZG2JN",
+      service_id: "service_n2zflqr",
+      template_id: "template_j31ncdi",
+      user_id: "3_tunqhcVdZ_1745E",
       template_params: {
-        "name": document.getElementById("name").value,
-        "email": document.getElementById("email").value,
-        "sentences": document.getElementById("sentences").value
-      }
-      }
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        sentences: document.getElementById("sentences").value,
+      },
+    };
 
     fetch("https://api.emailjs.com/api/v1.0/email/send", {
-      method: "POST", 
+      method: "POST",
       headers: {
-        "Content-Type": "application/json"
-      }, 
-      body: JSON.stringify(data)
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
     })
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
-
-  })
-
-  document.getElementById("send").addEventListener("click", () => {
-    emailjs.send("contact_service", "contact_form", data).then((res) => {
-      console.log("test");
-    });
+      .then((res) => {
+        res.status === 200
+          ? Swal.fire({
+              icon: "success",
+              title: "Success",
+              text: "Email sent successfully",
+            })
+          : Swal.fire({
+              icon: "error",
+              title: "Oops...",
+              text: "Something went wrong!",
+            });
+      })
+      .catch((e) => {
+        console.error(e);
+      });
   });
 };
 
