8350a3fdd355b4e2de851d33a67df1fc7f757c82
syafiqfadillah <codingan77@gmail.com>
2022-10-23 07:16:18 +0700

    new implementation

--- src/index.js
+++ src/index.js
@@ -6,11 +6,6 @@ const main = () => {
   // deklarasi variabel untuk mengambil element dari html
   const menu = document.querySelector(".ri-menu-line");
   const list = document.querySelector(".nav-list");
-	const data = {
-    name: document.getElementById("name").value,
-    email: document.getElementById("email").value,
-    sentences: document.getElementById("sentences").value
-  }
 
   // Event listener ketika menu di klik
   menu.addEventListener("click", () => {
@@ -18,13 +13,30 @@ const main = () => {
     menu.classList.toggle("black");
     document.body.classList.toggle("hidden");
   });
-	
-	document.getElementById("send").addEventListener("click", () => {
 
-  emailjs.send("contact_service", "contact_form", data).then(res => {
-      console.log("test");
+	document.getElementById("send").addEventListener("click", () => {
+    const data = {
+      service_id: "contact_service",
+      template_id: "contact_form",
+      user_id: "l2XE1auQUF2SZG2JN",
+      template_params: {
+        "name": document.getElementById("name").value,
+        "email": document.getElementById("email").value,
+        "sentences": document.getElementById("sentences").value
+      }
+      }
+
+    fetch("https://api.emailjs.com/api/v1.0/email/send", {
+      method: "POST", 
+      headers: {
+        "Content-Type": "application/json"
+      }, 
+      body: JSON.stringify(data)
+    })
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+
   })
-})
 };
 
 // Jalankan fungi main
